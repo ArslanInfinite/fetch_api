a1aab0740bb20f871c17dcca1fc925edbd8e10b5
Arslan <ali.arslan319@gmail.com>
2021-10-06 23:58:33 -0400

    use getText function to retrieve data from test.txt file

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ document.getElementById('button1').addEventListener('click', getText)
 
 function getText(){
     fetch('test.txt')
+    .then(function(response){
+        console.log(response)
+    })
 }
